363897d4ed9ef29cb2c1da3ea7986b3d2a21e960
Cristian Murillo <cam_725@hotmail.com>
2016-12-26 11:35:34 -0500

    Update 6.soloActualizarES.bat

--- 6.soloActualizarES.bat
+++ 6.soloActualizarES.bat
@@ -3,10 +3,10 @@ REM @Creado por: Ing. Cristian Andres Murillo A. Esp. en SIG
 REM @Drescripcion: Script para actualizacion de las tablas de
 REM lados de manzana, predios y terrenos en el aplicativo SIG ESTRATIFICACION
 
-SET PGPASSWORD=adminestra10A
-SET user=postgres
-SET dbname=bd_estratificacion
-set es=E:\es_DIC_16_2016.csv
+SET PGPASSWORD=**
+SET user=**
+SET dbname=**
+set es=E:\**.csv
 
 "C:\Program Files\PostgreSQL\9.3\bin\psql.exe" -U %user% -d %dbname% -c "DROP VIEW IF EXISTS public.terreno_estrato;"
 "C:\Program Files\PostgreSQL\9.3\bin\psql.exe" -U %user% -d %dbname% -c "select actualizar_lados('%es%',';');"
